22baa64d5df100de86b06abe0797a30de7ac856b
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-01 01:49:50 +0530

    ..

--- main.go
+++ main.go
@@ -35,6 +35,31 @@ func main(){
 
 
 func crawl(month int , day int){
+	c:=colly.NewCollector(
+		colly.AllowedDomains("imdb.com","www.imdb.com")
+	)
 
-	
+	infoCollector:=c.Clone()
+
+
+	c.OnHTML(".mode-detail",func(e *colly.HTMLElement)){
+		profileUrl:=e.ChildAttr("div.lister-item-image > a","href")
+		profileUrl=e.Request.AbsoluteURL(profileUrl)
+		infoCollector.Visit(profileUrl)
+	}
+
+	c.OnHTML("a.lister-page-next",func(e *colly.HTMLElement){
+		nextPage:=e.Request.AbsoluteURL(e.Attr("href"))
+		c.Visit(nextPage)
+	})
+
+	infoCollector.OnHTML("#")
+
+
+	startUrl:=fmt.Sprintf("https://www.imdb.com/search/name/?birth_monthday=%d-%d",month,day)
+	c.Visit(startUrl)
+
+
+
+
 }
